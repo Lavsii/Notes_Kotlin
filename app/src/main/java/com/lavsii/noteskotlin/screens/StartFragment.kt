35a90ef65d98c99dd35cfd410d2ad36293918f38
Lavsii <dmitry.oblog@gmail.com>
2022-11-18 09:11:02 +0300

    init project

---
+++ app/src/main/java/com/lavsii/noteskotlin/screens/StartFragment.kt
@@ -0,0 +1,19 @@
+package com.lavsii.noteskotlin.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lavsii.noteskotlin.R
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+}
